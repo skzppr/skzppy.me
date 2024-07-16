01c1dccc32c0b5adb236dfd6930d8c27db77b62d
Skippy <skippertubehd@gmail.com>
2024-07-16 12:27:43 -0500

    updated icons, bg
    - added twitter link

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // Parallax effect
-    document.addEventListener('mousemove', function(e) {
-        const x = (e.clientX / window.innerWidth) - 0.5;
-        const y = (e.clientY / window.innerHeight) - 0.5;
+    window.addEventListener('scroll', function() {
+        const scrolled = window.scrollY;
+        const parallax = scrolled * 0.5;
         
-        document.body.style.backgroundPosition = `${50 + x * 10}% ${50 + y * 10}%`;
+        document.body.style.backgroundPosition = `50% ${50 + parallax}px`;
     });
 });
